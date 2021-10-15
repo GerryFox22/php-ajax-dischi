32596c4ac81cb748760a4d71de76df084d9da78d
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-10-15 14:39:54 +0200

    Ultime modifiche e termine esercizio

--- db/database.php
+++ db/database.php
@@ -9,14 +9,14 @@
             "year"=>"1988"
          ],
          [
-            "poster"=>"https:\/\/img.discogs.com\/vknPDdrqRbT92pNRX0W4I5N91jg=\/fit-in\/300x300\/filters:strip_icc():format(jpeg):mode_rgb():quality(40)\/discogs-images\/R-1246953-1448927086-6590.jpeg.jpg",
+            "poster"=>"https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._AC_SX450_.jpg",
             "title"=>"Live at Wembley 86",
             "author"=>"Queen",
             "genre"=>"Pop",
             "year"=>"1992"
          ],
          [
-            "poster"=>"https:\/\/images-na.ssl-images-amazon.com\/images\/I\/41JD3CW65HL.jpg",
+            "poster"=>"https://images-eu.ssl-images-amazon.com/images/I/411BQR6BHRL.__AC_QL70_ML2_.jpg",
             "title"=>"Ten's Summoner's Tales",
             "author"=>"Sting",
             "genre"=>"Pop",
@@ -30,7 +30,7 @@
             "year"=>"2018"
          ],
          [
-            "poster"=>"https:\/\/images-na.ssl-images-amazon.com\/images\/I\/810nSIQOLiL._SY355_.jpg",
+            "poster"=>"https://img.discogs.com/ZyRehiDiEs7EsEEKe6PRackT5o4=/fit-in/600x600/filters:strip_icc():format(webp):mode_rgb():quality(90)/discogs-images/R-1355871-1281401669.jpeg.jpg",
             "title"=>"Brave new World",
             "author"=>"Iron Maiden",
             "genre"=>"Metal",
@@ -44,28 +44,28 @@
             "year"=>"2002"
          ],
          [
-            "poster"=>"https:\/\/images-na.ssl-images-amazon.com\/images\/I\/51rggtPgmRL.jpg",
+            "poster"=>"https://futura.fm/wp-content/uploads/2020/06/15350696491_ca9f7659db_b.jpg",
             "title"=>"Made in Japan",
             "author"=>"Deep Purple",
             "genre"=>"Rock",
             "year"=>"1972"
          ],
          [
-            "poster"=>"https:\/\/images-na.ssl-images-amazon.com\/images\/I\/81r3FVfNG3L._SY355_.jpg",
+            "poster"=>"https://images-na.ssl-images-amazon.com/images/I/51rxl5GsdML._AC_SX425_.jpg",
             "title"=>"And Justice for All",
             "author"=>"Metallica",
             "genre"=>"Metal",
             "year"=>"1988"
          ],
          [
-            "poster"=>"https:\/\/img.discogs.com\/KOBsqQwKiNKH-q927oHMyVdDzSo=\/fit-in\/596x596\/filters:strip_icc():format(jpeg):mode_rgb():quality(90)\/discogs-images\/R-6406665-1418464475-6120.jpeg.jpg",
+            "poster"=>"https://img.discogs.com/9Wpfc4Y5G8nT1I-H-eqG3-SJwZk=/fit-in/596x596/filters:strip_icc():format(webp):mode_rgb():quality(90)/discogs-images/R-2173446-1296926502.jpeg.jpg",
             "title"=>"Hard Wired",
             "author"=>"Dave Weckl",
             "genre"=>"Jazz",
             "year"=>"1994"
          ],
          [
-            "poster"=>"https:\/\/m.media-amazon.com\/images\/I\/71K9CbNZPsL._SS500_.jpg",
+            "poster"=>"https://images-na.ssl-images-amazon.com/images/I/6148jblbNqL._AC_SL1500_.jpg",
             "title"=>"Bad",
             "author"=>"Michael Jacjson",
             "genre"=>"Pop",
--- index.php
+++ index.php
@@ -4,36 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <?php 
-        include_once __DIR__ . "/db/database.php";
-        include_once __DIR__ . "/css/style.css";
-    ; ?>
-    
-    <?php foreach($dischi as $disco) {; ?>
-        <div class="disco">
+    <?php include_once __DIR__ . "/db/database.php"; ?>
+    <style>
+        <?php include_once __DIR__ . "/css/style.css"; ?>
+    </style>
+    <div class="container p-5">
+        <div class="row row-cols-lg-5">
+                <?php foreach($dischi as $disco) {; ?>
+                    <div class=" disco">
+                    
+                        <img src="<?php echo $disco["poster"] ; ?>" alt="my image">
+                        
 
-            <img src="<?php echo $disco["poster"] ; ?>" alt="my image">
-            
+                        <h1>
+                            <?php echo $disco["title"]; ?>
+                        </h1>
 
-            <h1>
-                <?php echo $disco["title"]; ?>
-            </h1>
+                        <p>
+                            <?php echo $disco["author"]; ?>
+                            
+                        </p>
 
-            <p>
-                <?php echo $disco["author"]; ?>
+                        <p>
+                            <?php echo $disco["year"]; ?>
+                            
+                        </p>
+                    </div>
                 
-            </p>
-
-            <p>
-                <?php echo $disco["year"]; ?>
-                
-            </p>
+                <?php }; ?>
         </div>
-        
-    <?php }; ?>
+    </div>
+    
 </body>
 </html>
